7272f274eee33f42607790a4f58b7dcfbf53c34a
viaDigitale <michael.pommerat.pro@gmail.com>
2025-05-05 23:34:00 +0200

    Update hero.ts

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
 export const heroDetails = {
     heading: 'Vos interclubs sans prises de tête !',
     subheading: 'La plateforme tout-en-un pour gérer vos compétitions interclubs. Pas besoin d’être ceinture noire en informatique.',
-    centerImageSrc: '/images/hero-mockup.webp',
+    centerImageSrc: '/images/mockup02_Tapis.png',
 }
