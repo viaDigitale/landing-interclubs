38bbe331d2bae5e53f97c13b5ec883b8bdaec513
viaDigitale <michael.pommerat.pro@gmail.com>
2025-05-05 22:12:06 +0200

    Update hero.ts

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
 export const heroDetails = {
-    heading: 'Smart, Secure, Simple Financial Management',
+    heading: 'Vos interclubs sans prises de tête !',
     subheading: 'From effortless budgeting to real-time investment insights, Finwise puts you in control of your money like never before',
     centerImageSrc: '/images/hero-mockup.webp',
 }
